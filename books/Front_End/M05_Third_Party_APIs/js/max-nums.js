ceb96124739c9e82b339168deb76a4325e00153d
Berry8833 <berryk8833@gmail.com>
2024-09-25 14:42:10 +0630

    adding M-5 Third party APIs

---
+++ books/Front_End/M05_Third_Party_APIs/js/max-nums.js
@@ -0,0 +1,21 @@
+var maxNum = function (arr) {
+    var max = arr[0];
+
+    for (var i = 1; i < arr.length; i++) {
+        var currentNum = arr[i];
+        if (currentNum > max) {
+            max = currentNum;
+        }
+    }
+
+    return max;
+}
+
+var expect = chai.expect;
+describe('maxNum', function() {
+    it('returns the largest number in a given array', function() {
+        var arr = [2, 3, -2, 99, 100, 5000, 321];
+        var result = maxNum(arr);
+        expect(result).to.eql(5000);
+    });
+});
